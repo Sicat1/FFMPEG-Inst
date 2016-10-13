62d00a5a711b81af289de17281f966ab6d4c58e9
Soundofdarkness <imsoundoflight@gmail.com>
2016-10-13 11:39:59 +0200

    Working Release v1.2

---
+++ FFMPEG Installer/Downloader.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.IO;
+using System.Net;
+
+namespace FFMPEG_Installer
+{
+    class Downloader
+    {
+        public static String x64Address = "http://eleria.de/ffmpeg64.exe";
+        public static String x32Address = "http://eleria.de/ffmpeg32.exe";
+
+        public static void x64Downloader()
+        {
+            WebClient Downloader = new WebClient();
+            Downloader.DownloadFile(x64Address, "ffmpeg.exe");
+            File.Move("ffmpeg.exe", "C://FFMPEG/ffmpeg.exe");
+        }
+        
+        public static void x32Downloader()
+        {
+            WebClient Downloader = new WebClient();
+            Downloader.DownloadFile(x32Address, "ffmpeg.exe");
+            File.Move("ffmpeg.exe", "C://FFMPEG/ffmpeg.exe");
+        }
+    }
+}
--- FFMPEG Installer/Program.cs
+++ FFMPEG Installer/Program.cs
@@ -1,15 +1,65 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.IO;
 
 namespace FFMPEG_Installer
 {
     class Program
     {
+        public static String FFMPEGPATH = "C://FFMPEG";
         static void Main(string[] args)
         {
+            Console.Title = "FFMPEG Installer by Eleria and Erein v1.2";
+            
+
+            Console.WriteLine("+--------------------------------------------------------+");
+            Console.WriteLine("|   FFMPEG Installer for NadekoBot by Eleria and Erein   |");
+            Console.WriteLine("+--------------------------------------------------------+");
+            Console.WriteLine("|                Do you want to install?                 |");
+            Console.WriteLine("|     1) Yes                                 2) No       |");
+            Console.WriteLine("| This will remove the Directory C:/ffmpeg if it exists  |");
+            Console.WriteLine("+--------------------------------------------------------+");
+            //Waiting for User Input:
+            var input = Console.ReadLine().Trim();
+
+            if(input == "Yes")
+            {
+                if(Directory.Exists(FFMPEGPATH))
+                {
+                    Console.WriteLine("C://ffmpeg exists already. Deleting...");
+                    Directory.Delete(FFMPEGPATH, true);
+                    Directory.CreateDirectory(FFMPEGPATH);
+                }
+                else
+                {
+                    Console.WriteLine("Creating C://FFMPEG...");
+                    Directory.CreateDirectory(FFMPEGPATH);
+                }
+                //Checking for Dir's done. Installer Part:
+
+                if (Environment.Is64BitOperatingSystem)
+                {
+                    Console.WriteLine("x64 Operating System detected.");
+                    Console.WriteLine("Downloading FFMPEG, this could take some time (no need for Coffee tho)");
+                    Downloader.x64Downloader();
+                }
+                else
+                {
+                    Console.WriteLine("x32 Operating System detected.");
+                    Console.WriteLine("Downloading FFMPEG, this could take some time (no need for Coffee tho)");
+                    Downloader.x32Downloader();
+                }
+                Console.WriteLine("Downloading done.");
+                Console.WriteLine("Adding FFMPEG to User Path...");
+                Environment.SetEnvironmentVariable("path", FFMPEGPATH, EnvironmentVariableTarget.User);
+                Console.WriteLine("Installation finished successfully. Press any Key to exit");
+                var i2 = Console.ReadLine().Trim();
+                return;
+            }
+            else
+            {
+                Console.WriteLine("Installation aborted.");
+                return;
+            }
         }
     }
 }
